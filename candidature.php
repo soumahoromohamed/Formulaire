4bf02d34a1e1c5c277d464bdef22ab7693315960
mohamedrayan <stayfocus2121@gmail.com>
2026-04-13 23:07:07 +0100

    feat: structure HTML et initialisation des variables

---
+++ candidature.php
@@ -0,0 +1,26 @@
+<?php
+$prenom    = '';
+$nom       = '';
+$email     = '';
+$age       = '';
+$filiere   = '';
+$motivation = '';
+$erreurs   = [];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de candidature</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+
+    </div>
+
+</body>
+</html>
